53320134582f4705945190def342f2080e9d77b9
Tam Vu <tam.vu@hitachivantara.com>
2022-11-30 15:44:46 +0700

    push CI/CD

---
+++ KinhThanhCongGiao/KinhThanhCongGiao.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace KinhThanhCongGiao.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
